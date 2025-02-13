b049e0c4aef712439554e3d61ddeee534c89d2e5
JheisonQuiroga <jheisonquiroga7@gmail.com>
2025-02-12 19:15:26 -0500

    First steps in JavaScript, Comments and add our first hello world

---
+++ Basic/00_hello_world.js
@@ -0,0 +1,21 @@
+// COMENTARIOS
+
+/*
+Como en todo lenguaje, existen dos tipos de comentarios.
+Los comentarios en una única línea y en multilínea (es decir un 
+comentario en muchas líneas)
+*/
+
+// Este es un comentario en una única línea de código.
+
+/*
+Este es un comentario
+de varias
+líneas de 
+código.
+*/
+
+
+// Nuestro primer Hola Mundo
+
+console.log("Hello World")
