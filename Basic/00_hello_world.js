1355fcf752b1351addc0811c1f867c7093485a3a
JheisonQuiroga <jheisonquiroga7@gmail.com>
2025-02-14 11:43:06 -0500

    Enhance documentation and examples in hello world script

--- Basic/00_hello_world.js
+++ Basic/00_hello_world.js
@@ -6,7 +6,19 @@ JavaScript es un lenguaje de programación orientado a objetos POO y de tipado
 dinámico.
 */
 
-// COMENTARIOS
+/* 2. DOCUMENTACIÓN
+2.1. Mozilla: https://developer.mozilla.org/es/docs/Web/JavaScript
+2.2. ECMAScript: https://tc39.es/ecma262/
+2.3. W3School: https://www.w3schools.com/js/default.asp
+*/
+
+/* 3. EXPLORADOR WEB
+Cualquier explorador WEB es capaz de ejecutar código JavaScript, todo por medio
+de los developer tools del navegador en el que te encuentres, o ingresando en el
+con el siguiente shortcut (Ctrl + Shift + J).
+*/
+
+// 4. COMENTARIOS
 
 /*
 Como en todo lenguaje, existen dos tipos de comentarios.
@@ -24,8 +36,39 @@ código.
 */
 
 
-// Nuestro primer Hola Mundo
+// 5. Nuestro primer Hola Mundo
+
+/* El siguiente código puede ser ejecutado desde la consola del navegador */
 
 console.log("Hello World")
 console.log("Hello", "World", "!")
 
+/* 6. INSTALACIÓN 
+NodeJS: Es un entorno de ejecución de JavaScript para probar código JavaScript por
+medio de la consola, es decir por fuera del explorador web.
+Link Instalación: https://nodejs.org/en/download
+
+Para ejecutar código JavaScript, utilizamos en la terminal el siguiente comando:
+
+$ node script.js
+*/
+
+/* 7. GUIA DE ESTILO DE GOOGLE
+Es una guia de estilo para seguir las buenas prácticas de desarrollo en el lenguaje
+de JavaScript, hace que el código sea más limpio, legible y escalable.
+https://google.github.io/styleguide/jsguide.html
+*/
+
+
+// 8. Más formas de utilizar la función console.log()
+
+console.log("5")
+console.log(5)
+// Utilizando operadores 
+console.log(5 + 5)
+console.log(3 - 2)
+console.log(3 * 3)
+console.log(3 / 3)
+console.log(3 / 2)
+console.log(5 % 2) // El resto de 5 / 2
+console.log(3 ** 2) // Tres al cuadrado
